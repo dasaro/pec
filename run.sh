e97495ddcae8a40cef50b1780538571dc7372977
Fabio D'Asaro <fabiodasaro@Fabios-MacBook.local>
2016-09-24 18:01:25 +0100

    Minor bug fixes

--- run.sh
+++ run.sh
@@ -11,22 +11,22 @@
 # DROPBOX_INSTALLATION_PATH="$(python -c "import json;f=open('$HOME/.dropbox/info.json', 'r').read();data=json.loads(f);print data.get('personal', {}).get('path', '')")/FabioPhDsupervision/PEC_Paper/pec-implementation"
 
 # OTHER PATH:
-OTHER_INSTALLATION_PATH="/Users/dasaro/Desktop/pec-git"
+OTHER_INSTALLATION_PATH="/Users/fabiodasaro/Desktop/pec_implem"
 
-if [ -n $DROPBOX_INSTALLATION_PATH ]; then
-  INSTALLATION_PATH=$DROPBOX_INSTALLATION_PATH
-  echo "\nUSING DROPBOX INSTALL: $INSTALLATION_PATH\n"
-else
+if [ -z $DROPBOX_INSTALLATION_PATH ]; then
   INSTALLATION_PATH=$OTHER_INSTALLATION_PATH
   echo "\nUSING INSTALLATION PATH: $INSTALLATION_PATH\n"
+else
+  INSTALLATION_PATH=$DROPBOX_INSTALLATION_PATH
+  echo "\nUSING DROPBOX INSTALL: $INSTALLATION_PATH\n"
 fi
 
 # BINARIES LOCATION
 # Use evaluation_script="Worlds_as_stable_models/evaluate2.sh";
 # if the independent part of pec supports autonomous probability evaluation
 translator="$INSTALLATION_PATH/Translator/bin/translator";
-pec_domain_independent="$INSTALLATION_PATH/Worlds_as_stable_models/pec.lp";
-evaluation_script="$INSTALLATION_PATH/Worlds_as_stable_models/evaluate2.sh";
+pec_domain_independent="$INSTALLATION_PATH/Worlds_as_stable_models/pec_alt.lp";
+evaluation_script="$INSTALLATION_PATH/Worlds_as_stable_models/evaluate_alt.sh";
 
 #
 # RUN PEC
