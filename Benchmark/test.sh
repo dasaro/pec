b33c5cd9623fba31aeda1e8846e361efb7a14f90
dasaro <fabiodasaro@fabios-macbook.home>
2017-05-14 22:22:39 +0100

    Benchmark script update

--- Benchmark/test.sh
+++ Benchmark/test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 for f in Test_domains/*
 do
-    SECONDS=0
-    { ../Translator/bin/translator < $f & cat ../Reasoner/pec.lp; } | clingo -n 0 &> /dev/null
-    echo "$SECONDS" >> Reports/report.txt
+{ ../Translator/bin/translator < $f & cat ../Reasoner/pec.lp; } | clingo -n 0 | awk '/^Time/ { total = $3; solving = $5; print total, "\t", solving }' >> Reports/Antibiotic/Total_and_solving_time.txt
 done
